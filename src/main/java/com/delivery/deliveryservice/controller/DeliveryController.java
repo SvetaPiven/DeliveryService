1d08e831f796f86d08497d7d874885e8b13cbe09
Maxim <mak.dragun2012@yandex.ru>
2023-09-30 23:23:31 +0300

    add business-logic create service and controller

--- src/main/java/com/delivery/deliveryservice/controller/DeliveryController.java
+++ src/main/java/com/delivery/deliveryservice/controller/DeliveryController.java
@@ -1,21 +1,45 @@
 package com.delivery.deliveryservice.controller;
 
-import com.delivery.deliveryservice.nebagafeature.KafKaConsumer;
+import com.delivery.deliveryservice.service.DeliveryService;
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RequestMapping("/api/delivery")
 @RestController
 @RequiredArgsConstructor
 public class DeliveryController {
 
-    private final KafKaConsumer kafKaConsumer;
+//    private final KafKaConsumer kafKaConsumer;
 
-    @GetMapping
-    public ResponseEntity<?> getMessage() {
-        return ResponseEntity.ok(kafKaConsumer.getMessageFromTopic());
+    //    @GetMapping
+//    public ResponseEntity<?> getMessage() {
+//        return ResponseEntity.ok(kafKaConsumer.getMessageFromTopic());
+//    }
+    private final DeliveryService deliveryService;
+
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<?> setStatus(@PathVariable("id") UUID uuid, @RequestBody String status) {
+        deliveryService.setStatus(uuid, status);
+        return ResponseEntity.
+                status(HttpStatus.ACCEPTED)
+                .build();
+    }
+
+    @GetMapping("/{id}/status")
+    public ResponseEntity<String> getStatus(@PathVariable("id") UUID uuid) {
+        return ResponseEntity.
+                status(HttpStatus.ACCEPTED)
+                .body(deliveryService.getStatus(uuid));
     }
 }
--- src/main/java/com/delivery/deliveryservice/nebagafeature/KafKaConsumer.java
+++ src/main/java/com/delivery/deliveryservice/nebagafeature/KafKaConsumer.java
@@ -1,25 +1,30 @@
 package com.delivery.deliveryservice.nebagafeature;
 
+import com.delivery.deliveryservice.service.DeliveryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
 
 @Service
+@RequiredArgsConstructor
 public class KafKaConsumer {
+    private final DeliveryService deliveryService;
 
     private static final String TOPIC_NAME = "ordersTopic";
 
     private static final String GROUP_ID = "drakonishe";
 
-    private LinkedList<String> messages = new LinkedList<>();
+//    private LinkedList<String> messages = new LinkedList<>();
 
     @KafkaListener(topics = TOPIC_NAME, groupId = GROUP_ID)
     public void consume(String message) {
-        messages.addLast(message);
+//        messages.addLast(message);
+        deliveryService.createDelivery(message);
     }
 
-    public String getMessageFromTopic() {
-        return messages.pollFirst();
-    }
+//    public String getMessageFromTopic() {
+//        return messages.pollFirst();
+//    }
 }
